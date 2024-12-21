be3be7ef02f03aeed9a93924b0fd8769219c9386
twero <weronika.tylman@edu.uekat.pl>
2024-12-21 22:00:37 +0100

    finished project

--- app/src/main/java/com/verticalcoding/mydoglist/MainActivity.kt
+++ app/src/main/java/com/verticalcoding/mydoglist/MainActivity.kt
@@ -3,54 +3,133 @@ package com.verticalcoding.mydoglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.*
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.verticalcoding.mydoglist.ui.theme.MyDogListTheme
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-data class Dog(val name: String, var isFavorite: Boolean = false)
+data class Dog(val name: String, val breed: String, var isFavorite: Boolean = false)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyDogListTheme {
-                DogListApp()
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "DogList") {
+                composable("DogList") {
+                    DogListApp(navController)
+                }
+                composable("DogScreen/{dogName}") { backStackEntry ->
+                    val dogName = backStackEntry.arguments?.getString("dogName") ?: "Unknown Dog"
+                    val dog = getDogByName(dogName)
+                    dog?.let {
+                        DogDetailsScreen(navController, it) { deletedDog ->
+                            dogList.remove(deletedDog)
+                            navController.popBackStack()
+                        }
+                    }
+                }
+                composable("Settings") {
+                    SettingsScreen(navController)
+                }
+                composable("Profile") {
+                    ProfileScreen(navController)
+                }
             }
         }
     }
+
+    private fun getDogByName(name: String): Dog? {
+        return dogList.find { it.name == name }
+    }
 }
 
+val dogList = mutableStateListOf(
+    Dog(name = "Bobby", breed = "Labrador"),
+    Dog(name = "Max", breed = "Beagle"),
+    Dog(name = "Rex", breed = "German Shepherd")
+)
+
+val dogNamesSet = mutableSetOf<String>()
+
 @Composable
-fun DogListApp() {
+fun DogListApp(navController: NavController) {
     var dogName by remember { mutableStateOf("") }
-    var dogs by remember { mutableStateOf(mutableSetOf<Dog>()) }
     var errorMessage by remember { mutableStateOf("") }
     var searchQuery by remember { mutableStateOf("") }
-    var favoriteCount by remember { mutableStateOf(0) }
 
-    Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+    val favoriteCount by remember { derivedStateOf { dogList.count { it.isFavorite } } }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    IconButton(onClick = { navController.navigate("Settings") }) {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+
+                    Text(
+                        text = "Doggos",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+
+
+                    IconButton(onClick = { navController.navigate("Profile") }) {
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Profile",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
 
             Row(
                 modifier = Modifier
@@ -79,7 +158,6 @@ fun DogListApp() {
 
                 Spacer(modifier = Modifier.width(8.dp))
 
-
                 IconButton(
                     onClick = {
                         searchQuery = dogName
@@ -91,13 +169,13 @@ fun DogListApp() {
 
                 Spacer(modifier = Modifier.width(8.dp))
 
-
                 IconButton(
                     onClick = {
-                        if (dogs.any { it.name.equals(dogName, ignoreCase = true) }) {
+                        if (dogNamesSet.contains(dogName.lowercase())) {
                             errorMessage = "Pies o tej nazwie już istnieje!"
                         } else {
-                            dogs = dogs.toMutableSet().apply { add(Dog(name = dogName)) }
+                            dogList.add(Dog(name = dogName, breed = "Unknown"))
+                            dogNamesSet.add(dogName.lowercase())
                             dogName = ""
                             errorMessage = ""
                         }
@@ -123,7 +201,6 @@ fun DogListApp() {
                     .padding(horizontal = 16.dp, vertical = 8.dp),
                 horizontalArrangement = Arrangement.Start
             ) {
-
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.Start
@@ -136,7 +213,7 @@ fun DogListApp() {
                     )
                     Spacer(modifier = Modifier.width(4.dp))
                     Text(
-                        text = ": ${dogs.size}",
+                        text = ": ${dogList.size}",
                         fontWeight = FontWeight.Bold,
                         fontSize = 16.sp
                     )
@@ -160,24 +237,26 @@ fun DogListApp() {
 
             LazyColumn(modifier = Modifier.padding(16.dp)) {
                 val filteredDogs = if (searchQuery.isEmpty()) {
-                    dogs
+                    dogList
                 } else {
-                    dogs.filter { it.name.contains(searchQuery, ignoreCase = true) }
+                    dogList.filter { it.name.contains(searchQuery, ignoreCase = true) }
                 }
 
                 items(filteredDogs.sortedByDescending { it.isFavorite }) { dog ->
                     DogItem(
                         dog = dog,
                         onDelete = {
-                            dogs = dogs.filter { it != dog }.toMutableSet()
-                            favoriteCount = dogs.count { it.isFavorite }
+                            dogList.remove(dog)
+                            dogNamesSet.remove(dog.name.lowercase())
                         },
                         onFavorite = { updatedDog ->
-                            dogs = dogs.map {
-                                if (it.name == updatedDog.name) it.copy(isFavorite = !it.isFavorite)
-                                else it
-                            }.toMutableSet()
-                            favoriteCount = dogs.count { it.isFavorite }
+                            val index = dogList.indexOfFirst { it.name == updatedDog.name }
+                            if (index != -1) {
+                                dogList[index] = updatedDog.copy(isFavorite = !updatedDog.isFavorite)
+                            }
+                        },
+                        onClick = {
+                            navController.navigate("DogScreen/${dog.name}")
                         }
                     )
                 }
@@ -186,24 +265,25 @@ fun DogListApp() {
     }
 }
 
+
+
+
 @Composable
-fun DogItem(dog: Dog, onDelete: () -> Unit, onFavorite: (Dog) -> Unit) {
+fun DogItem(dog: Dog, onDelete: () -> Unit, onFavorite: (Dog) -> Unit, onClick: () -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .padding(16.dp),
+            .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(
                     brush = Brush.linearGradient(
                         colors = listOf(Color(0xFFEEB6E9), Color(0xFF65558F))
-                    ),
-                    shape = MaterialTheme.shapes.small
+                    )
                 )
         ) {
             Icon(
@@ -216,40 +296,242 @@ fun DogItem(dog: Dog, onDelete: () -> Unit, onFavorite: (Dog) -> Unit) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Text(
-            text = dog.name,
-            fontWeight = FontWeight.Bold,
+        Column(
             modifier = Modifier.weight(1f)
-        )
+        ) {
+            Text(
+                text = dog.breed,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = dog.name,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp
+            )
+        }
 
-        IconButton(onClick = {
-            onFavorite(dog)
-        }) {
+        Spacer(modifier = Modifier.width(8.dp))
 
-            Box(
-                modifier = Modifier
-                    .size(24.dp)
-                    .graphicsLayer {
-                        shadowElevation = 4.dp.toPx()
-                    }
-                    .background(
-                        brush = Brush.linearGradient(
-                            colors = listOf(Color(0xFFEEB6E9), Color(0xFF65558F))
-                        ),
-                        shape = MaterialTheme.shapes.small
-                    )
-            ) {
+        IconButton(onClick = { onFavorite(dog) }) {
+            Icon(
+                imageVector = if (dog.isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                contentDescription = "Favorite"
+            )
+        }
+
+        IconButton(onClick = { onDelete() }) {
+            Icon(
+                imageVector = Icons.Default.Delete,
+                contentDescription = "Delete"
+            )
+        }
+    }
+}
+
+
+@Composable
+fun DogDetailsScreen(navController: NavController, dog: Dog, onDelete: (Dog) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            IconButton(onClick = {
+                if (navController.previousBackStackEntry != null) {
+                    navController.popBackStack()
+                } else {
+                    navController.navigate("DogList")
+                }
+            }) {
                 Icon(
-                    Icons.Default.Favorite,
-                    contentDescription = "Add to Favorite",
-                    modifier = Modifier.matchParentSize(),
-                    tint = if (dog.isFavorite) Color.Transparent else Color.Gray
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Text(
+                text = "Detale",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+
+            Spacer(modifier = Modifier.weight(1f))
+
+
+            IconButton(onClick = {
+                onDelete(dog)
+                navController.navigate("DogList")
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Dog",
+                    tint = Color.Black
                 )
             }
         }
 
-        IconButton(onClick = onDelete) {
-            Icon(Icons.Default.Delete, contentDescription = "Usuń psa", tint = Color.Black)
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Color(0xFFEEB6E9), Color(0xFF65558F))
+                    )
+                )
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.dog),
+                contentDescription = "Dog Icon",
+                modifier = Modifier.align(Alignment.Center).size(100.dp),
+                tint = Color.White
+            )
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = dog.breed,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = dog.name,
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
+
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+
+            IconButton(onClick = {
+                if (navController.previousBackStackEntry != null) {
+                    navController.popBackStack()
+                } else {
+                    navController.navigate("DogList")
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = "Ustawienia",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+
+            IconButton(onClick = {
+                if (navController.previousBackStackEntry != null) {
+                    navController.popBackStack()
+                } else {
+                    navController.navigate("DogList")
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = "Profil",
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+                .background(Color.Gray)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Profile Icon",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(80.dp),
+                tint = Color.White
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Jan Brzechwa",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
